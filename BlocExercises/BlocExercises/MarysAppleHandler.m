abea474dbd7ea54f9eefedbf7e9a6a69dac0637e
Michelle Rutherford <michellewwg@gmail.com>
2015-04-02 20:39:28 -0400

    Completed if , else , ternary

--- BlocExercises/BlocExercises/MarysAppleHandler.m
+++ BlocExercises/BlocExercises/MarysAppleHandler.m
@@ -12,7 +12,25 @@
 
 - (NSString *) itemMaryCanPurchaseForDollars:(NSInteger)dollars {
     NSString *itemToReturn;
-
+    // ********************************************************
+    // *  I would normally haveused a switch statement for this
+    // * ******************************************************
+    if (dollars == 4)
+    {
+        itemToReturn = @"get out of my store";
+    } else if (dollars == 5)
+    {
+        itemToReturn = @"have some gum";
+    } else if (dollars == 6)
+    {
+        itemToReturn = @"have an apple";
+    } else if (dollars == 1000)
+    {
+        itemToReturn = @"have an Apple computer";
+    } else if (dollars == 1000000000)
+    {
+        itemToReturn =  @"have The Big Apple";
+    }
     /* WORK HERE */
     
     NSLog(@"For $%ld, Mary can: %@", (long)dollars, itemToReturn);
@@ -23,10 +41,10 @@
     /* WORK HERE */
 
     NSUInteger cost = 24;
-    
-    if (self.getsDiscount) {
-        cost *= .75;
-    }
+    cost = self.getsDiscount ? cost *= .75 : 24;
+//    if (self.getsDiscount) {
+//        cost *= .75;
+//    }
     
     return cost;
 }

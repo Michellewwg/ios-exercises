2e712cab81aaa8869e079096df6cbab6a3ea1ef8
Michelle Rutherford <michellewwg@gmail.com>
2015-04-07 21:56:53 -0400

    Checkpoint 18

---
+++ Alcolator2/Alcolator2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Alcolator2
+//
+//  Created by Michelle Rutherford on 4/7/15.
+//  Copyright (c) 2015 Michelle Rutherford. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Alcolator2/Alcolator2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Alcolator2
+//
+//  Created by Michelle Rutherford on 4/7/15.
+//  Copyright (c) 2015 Michelle Rutherford. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

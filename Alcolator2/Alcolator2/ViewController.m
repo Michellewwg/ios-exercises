9a64ec36b31f4298e16c7f91bf21c61df2c4d35f
Michelle Rutherford <michellewwg@gmail.com>
2015-04-07 21:29:56 -0400

    Assignment 18 before assignment complete

---
+++ Alcolator2/Alcolator2/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Alcolator2
+//
+//  Created by Michelle Rutherford on 4/7/15.
+//  Copyright (c) 2015 Michelle Rutherford. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Alcolator2/Alcolator2/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Alcolator2
+//
+//  Created by Michelle Rutherford on 4/7/15.
+//  Copyright (c) 2015 Michelle Rutherford. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}

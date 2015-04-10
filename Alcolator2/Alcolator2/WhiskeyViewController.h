cf3e96664962ea6fe722bc899e7a00381ce698c4
Michelle Rutherford <michellewwg@gmail.com>
2015-04-09 22:38:39 -0400

    Assignment 19 complete

---
+++ Alcolator2/Alcolator2/WhiskeyViewController.h
@@ -0,0 +1,13 @@
+//
+//  WhiskeyViewController.h
+//  Alcolator2
+//
+//  Created by Michelle Rutherford on 4/9/15.
+//  Copyright (c) 2015 Michelle Rutherford. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface WhiskeyViewController : ViewController
+
+@end
